644f6217882ccd06dbb5c9201a2d04a93d967be5
nanastiuk <nanastiuk@gbsfo.com>
2022-05-23 23:39:46 +0300

    created getter/setter generators #2

--- internal/models/todo_todoitem_gen_v2.go
+++ internal/models/todo_todoitem_gen_v2.go
@@ -1,41 +1,41 @@
 package models
 
-func (m *TodoItem) GetId() int {
+func (m *TodoItem) GetIdV2() int {
 	return m.Id
 }
 
-func (m *TodoItem) SetId(val int) {
+func (m *TodoItem) SetIdV2(val int) {
 	m.Id = val
 }
 
-func (m *TodoItem) GetTitle() string {
+func (m *TodoItem) GetTitleV2() string {
 	return m.Title
 }
 
-func (m *TodoItem) SetTitle(val string) {
+func (m *TodoItem) SetTitleV2(val string) {
 	m.Title = val
 }
 
-func (m *TodoItem) GetDescription() string {
+func (m *TodoItem) GetDescriptionV2() string {
 	return m.Description
 }
 
-func (m *TodoItem) SetDescription(val string) {
+func (m *TodoItem) SetDescriptionV2(val string) {
 	m.Description = val
 }
 
-func (m *TodoItem) GetStatus() string {
+func (m *TodoItem) GetStatusV2() string {
 	return m.Status
 }
 
-func (m *TodoItem) SetStatus(val string) {
+func (m *TodoItem) SetStatusV2(val string) {
 	m.Status = val
 }
 
-func (m *TodoItem) GetDone() bool {
+func (m *TodoItem) GetDoneV2() bool {
 	return m.Done
 }
 
-func (m *TodoItem) SetDone(val bool) {
+func (m *TodoItem) SetDoneV2(val bool) {
 	m.Done = val
 }
